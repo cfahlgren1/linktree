3093104fe005a29942b2bddb961567a9c5e6f40d
Caleb Fahlgren <cfahlgren1@gmail.com>
2020-10-20 14:04:53 -0500

    added resume to links

--- src/handlers/api.js
+++ src/handlers/api.js
@@ -4,7 +4,8 @@ export const data = {
         { "name": "LinkedIn", "url": "https://www.linkedin.com/in/caleb-fahlgren-8a8ba0170/" },
         { "name": "GitHub", "url": "https://github.com/cfahlgren1" },
         { "name": "Bulma", "url": "https://bulma.io/" },
-        { "name": "Cloudflare", "url": "https://www.cloudflare.com/" }
+        { "name": "Cloudflare", "url": "https://www.cloudflare.com/" },
+        { "name": "Resume", "url": "https://drive.google.com/file/d/1Uk6KWydB84B6rfsW9361WUB7g_yd5evV/view?usp=sharing"}
     ]
 }
 
